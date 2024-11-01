e37ae678d678e696af62b93cf003cbba2f2ba4e1
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2024-11-01 21:26:29 +0100

    build: optimisation

--- build.js
+++ build.js
@@ -1,17 +1,30 @@
 const spawn = require("cross-spawn");
-const fs = require("fs");
-var base_url = "/";
 
+const options = {
+  optimisation: false
+};
+
+const args = process.argv.slice(2);
+args.forEach(arg => {
+  if (arg.startsWith('--')) {
+    const option = arg.replace('--', '');
+    options[option] = true;
+  }
+});
+
+let baseURL = "/";
+// Configure the base URL for deployment on CF Pageswith support of Preview environment
 if (process.env.CF_PAGES_BRANCH === "main" || process.env.CF_PAGES_BRANCH === "master") {
-  base_url = "https://ludovicwyffels.dev/";
+  baseURL = "https://ludovicwyffels.dev/";
 } else if (process.env.CF_PAGES_URL) {
-  base_url = process.env.CF_PAGES_URL;
+  baseURL = process.env.CF_PAGES_URL;
 }
 
-console.log(`Using base url "${base_url}"`);
+console.log('Building ...')
+console.log(`Using base URL: "${baseURL}"`);
 cmd = spawn.sync(
   "hugo",
-  ["--cleanDestinationDir", "--minify", "-b", base_url],
+  ["--cleanDestinationDir", "--minify", "-b", baseURL],
   { encoding: "utf8" },
 );
 
@@ -22,18 +35,21 @@ if (cmd.error) {
 console.log(cmd.stdout);
 console.error(cmd.stderr);
 
-console.log("Optimizes static websites");
-cmd = spawn.sync(
-  "npx",
-  ["@divriots/jampack", "./public"],
-  { encoding: "utf8" },
-);
-
-if (cmd.error) {
-  console.log("ERROR: ", cmd.error);
+if (options.optimisation) {
+  console.log("Optimizes static websites");
+  cmd = spawn.sync(
+    "npx",
+    ["@divriots/jampack", "./public"],
+    { encoding: "utf8" },
+  );
+  
+  if (cmd.error) {
+    console.log("ERROR: ", cmd.error);
+  }
+  
+  console.log(cmd.stdout);
+  // console.error(cmd.stderr);
 }
 
-console.log(cmd.stdout);
-console.error(cmd.stderr);
 
 process.exit(cmd.status);
